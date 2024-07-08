2573bd1cd34f0a3c7a1140581cc28ba845d13e06
Max Schmide <kuznetsov.max.v@gmail.com>
2024-07-08 21:04:42 +0300

    web: setup shadcn/ui

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from 'tailwindcss'
 
-const config: Config = {
+const config = {
   content: ['./src/**/*.{ts,tsx}'],
+  prefix: '',
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config
 
 export default config
