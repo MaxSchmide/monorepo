a39ccbccc3b6775e6e7305ee30e53dee33bcfa53
Max Schmide <kuznetsov.max.v@gmail.com>
2025-02-04 18:58:05 +0200

    fix module resolution

--- apps/landing/tailwind.config.ts
+++ apps/landing/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import animate from 'tailwindcss-animate'
 
 const config = {
   content: ['./src/**/*.{ts,tsx}'],
@@ -6,7 +7,7 @@ const config = {
   theme: {
     extend: {},
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [animate],
 } satisfies Config
 
 export default config
